9de0aa606fdb405bcc0c471e1e5da32e1a4c7488
Quang Phan <oclockvn@gmail.com>
2025-03-22 14:03:30 +0700

    add config

--- src/mega.UI/vite.config.ts
+++ src/mega.UI/vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
       // https://github.com/tabler/tabler-icons/issues/1233
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
+    preserveSymlinks: true,
   },
 })
