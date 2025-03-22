3f53ee84b5a9e13bb417fe544e3246864c0baaeb
Quang Phan <oclockvn@gmail.com>
2025-03-22 22:11:28 +0700

    add router

--- src/MegaUI/vite.config.ts
+++ src/MegaUI/vite.config.ts
@@ -4,10 +4,11 @@ import tailwindcss from "@tailwindcss/vite"
 
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin(), tailwindcss()],
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), plugin(), tailwindcss(),  ],
   server: {
     port: 54547,
   },
