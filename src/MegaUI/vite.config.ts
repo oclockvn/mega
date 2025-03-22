fe4e039b05ec3d62f78eb97bb26aa23f2c201f47
Quang Phan <oclockvn@gmail.com>
2025-03-22 14:49:35 +0700

    install shadcn

--- src/MegaUI/vite.config.ts
+++ src/MegaUI/vite.config.ts
@@ -1,10 +1,24 @@
+// import path from 'path'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 54547,
+  plugins: [plugin(), tailwindcss()],
+  server: {
+    port: 54547,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+
+      // fix loading all icon chunks in dev mode
+      // https://github.com/tabler/tabler-icons/issues/1233
+      //   '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
+    preserveSymlinks: true,
+  },
 })
