828f819780bf46415bf14e4f2da0017024e14b58
Quang Phan <oclockvn@gmail.com>
2025-05-03 21:32:43 +0700

    add vite app

--- src/mega.AppHost/Program.cs
+++ src/mega.AppHost/Program.cs
@@ -1,11 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.mega_Api>("mega-api").WithExternalHttpEndpoints();
+var apiService = builder.AddProject<Projects.mega_Api>("mega-api").WithHttpEndpoint(name: "api-http").WithExternalHttpEndpoints();
 
-var clientApp = builder.AddPnpmApp("mega-client", "../mega.ClientApp", "dev")
+//var _clientApp = builder.AddPnpmApp("mega-client", "../mega.ClientApp", "dev")
+//    .WithReference(apiService)
+//    .WithEnvironment("BROWSER", "none")
+//    .WithHttpEndpoint(env: "VITE_PORT")
+//    .WithExternalHttpEndpoints()
+//    .PublishAsDockerFile();
+
+var clientApp = builder.AddViteApp("mega-client", "../mega.ClientApp", "pnpm")
     .WithReference(apiService)
     .WithEnvironment("BROWSER", "none")
-    .WithHttpEndpoint(env: "VITE_PORT")
+    .WithHttpEndpoint(name: "client-http", env: "VITE_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
